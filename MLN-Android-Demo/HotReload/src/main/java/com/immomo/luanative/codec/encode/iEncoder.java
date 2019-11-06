bd1b864cf9e6219fcad69cac3e1406a1d451f56d
wu.tianlong <wu.tianlong@immomo.com>
2019-11-06 10:32:57 +0800

    feature:通过远程依赖方式  依赖MLN

--- MLN-Android-Demo/HotReload/src/main/java/com/immomo/luanative/codec/encode/iEncoder.java
+++
@@ -1,5 +0,0 @@
-package com.immomo.luanative.codec.encode;
-
-public interface iEncoder {
-    public byte[] encode(Object msg);
-}
--- MLN-Android-Demo/mlnservics/src/main/java/com/immomo/mls/adapter/ScriptReaderCreator.java
+++
@@ -1,9 +0,0 @@
-package com.immomo.mls.adapter;
-
-/**
- * Created by Xiong.Fangyu on 2018/11/13
- */
-public interface ScriptReaderCreator {
-
-    ScriptReader newScriptLoader(String src);
-}
