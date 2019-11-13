62f1c5115673e952b9307b2cbc5b8e4ee5183404
zhang.ke <zhang.ke@immomo.com>
2019-11-13 15:51:29 +0800

    refatcor: 合并 MLN美丽说 和 原生美丽说工程。

--- MLN-Android-Demo/sample/src/main/java/com/mln/demo/LauncherActivity.java
+++ MLN-Android-Demo/sample/src/main/java/com/mln/demo/LauncherActivity.java
@@ -1,15 +1,12 @@
 package com.mln.demo;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.TextView;
 
+import com.immomo.mls.InitData;
+import com.immomo.mls.MLSBundleUtils;
 import com.mln.demo.android.activity.MainTabActivity;
-import com.mln.demo.mln.activity.LuaViewActivity;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,41 +16,29 @@ import androidx.appcompat.app.AppCompatActivity;
  * on 2019-11-13
  */
 public class LauncherActivity extends AppCompatActivity implements View.OnClickListener {
-    private Switch switchbtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        switchbtn = findViewById(R.id.mln_native_switch);
-        final TextView btn = findViewById(R.id.mln_native_btn);
-
-        switchbtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    btn.setText("MLN 美丽说");
-                    App.getApp().initMLN();
-                } else {
-                    btn.setText("Native 美丽说");
-                }
-            }
-        });
-        btn.setOnClickListener(this);
+        findViewById(R.id.mln_btn).setOnClickListener(this);
+        findViewById(R.id.native_btn).setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.mln_native_btn:
-                if (switchbtn.isChecked()) {
-                    startActivity(new Intent(this, LuaViewActivity.class));
-                } else {
-                    startActivity(new Intent(this, MainTabActivity.class));
-                }
+            case R.id.mln_btn:
+                Intent intent = new Intent(this, com.immomo.mls.activity.LuaViewActivity.class);
+                String file = "file://android_asset/MMLuaKitGallery/meilishuo.lua";
+                InitData initData = MLSBundleUtils.createInitData(file);
+                intent.putExtras(MLSBundleUtils.createBundle(initData));
+                startActivity(intent);
+                break;
+            case R.id.native_btn:
+                startActivity(new Intent(this, MainTabActivity.class));
                 break;
         }
     }
