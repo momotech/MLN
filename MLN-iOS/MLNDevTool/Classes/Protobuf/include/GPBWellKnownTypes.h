0207adaa4fb74de9018886e33ce489eca7be3823
liu.xu_1586 <liu.xu_1586@immomo.com>
2019-12-31 10:13:03 +0800

    修改Protobuf路径

--- MLN-iOS/MLNDevTool/Classes/Protobuf/include/GPBWellKnownTypes.h
+++ MLN-iOS/MLNDevTool/Classes/Protobuf/include/GPBWellKnownTypes.h
@@ -41,9 +41,9 @@
  #import <Protobuf/Duration.pbobjc.h>
  #import <Protobuf/Timestamp.pbobjc.h>
 #else
- #import "google/protobuf/Any.pbobjc.h"
- #import "google/protobuf/Duration.pbobjc.h"
- #import "google/protobuf/Timestamp.pbobjc.h"
+ #import "Any.pbobjc.h"
+ #import "Duration.pbobjc.h"
+ #import "Timestamp.pbobjc.h"
 #endif
 
 NS_ASSUME_NONNULL_BEGIN
