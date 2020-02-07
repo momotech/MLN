9dd14fda3696e2432dbe38cb9eeedc83255e40b5
dingpuyu <771005227@qq.com>
2020-02-07 19:49:20 +0800

    fix:处理遍历过程中，对隐藏视图的过滤

--- MLN-iOS/MLN/Classes/Kit/Util/Gesture/MLNGestureRecognizer.m
+++ MLN-iOS/MLN/Classes/Kit/Util/Gesture/MLNGestureRecognizer.m
@@ -78,7 +78,7 @@
     for (UIView *subview in superView.subviews)
     {
         //当触摸点在子视图上时，才做处理
-        if (subview.lua_enable && subview.alpha > 0.01 && CGRectContainsPoint(subview.frame, point)) {
+        if (subview.hidden == NO && subview.lua_enable && subview.alpha > 0.01 && CGRectContainsPoint(subview.frame, point)) {
             if ( [subview lua_consumeEvent]) {
                 return YES;
             }
