c15cca7f07d567e0eee04306df86009d484c9934
dai.dongpeng <swiftwithlove@163.com>
2020-03-05 21:47:34 +0800

    [DataBinding] add unit tests

---
+++ MLN-iOS/Example/Tests/DataBinding/MLNTestModel.m
@@ -0,0 +1,14 @@
+//
+//  MLNTestModel.m
+//  MLN_Tests
+//
+//  Created by Dai Dongpeng on 2020/3/5.
+//  Copyright © 2020 liu.xu_1586. All rights reserved.
+//
+
+#import "MLNTestModel.h"
+
+@implementation MLNTestModel
+
+
+@end
