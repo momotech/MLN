981c4ad92c657a380ccf905c1ed8e944e91cc100
dai.dongpeng <swiftwithlove@163.com>
2020-03-17 14:42:24 +0800

    [DataBind] chang class name

--- MLN-iOS/Example/MLN/DataBind/MLNDataBindHotReload.h
+++ MLN-iOS/Example/MLN/DataBind/MLNDataBindHotReload.h
@@ -1,5 +1,5 @@
 //
-//  MLNDataBindViewController.h
+//  MLNDataBindHotReload.h
 //  LuaNative
 //
 //  Created by Dai Dongpeng on 2020/3/10.
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MLNDataBindViewController : MLNHotReloadViewController
+@interface MLNDataBindHotReload : MLNHotReloadViewController
 
 @end
 
