5c01de7a85da24791d6acfc8f5b2e9fe84ec531b
dong.xin <dong.xin@immomo.com>
2020-04-07 14:49:51 +0800

    fix lua_children method

--- MLN-iOS/MLN/Classes/Kit/Layout/UI/Stack/MLNStack.m
+++ MLN-iOS/MLN/Classes/Kit/Layout/UI/Stack/MLNStack.m
@@ -30,6 +30,9 @@
 #pragma mark - Export Lua
 
 - (void)lua_children:(NSArray<UIView *> *)subviews {
+    if ([subviews isKindOfClass:[NSArray class]] == NO) {
+        return;
+    }
     [subviews enumerateObjectsUsingBlock:^(UIView *_Nonnull view, NSUInteger idx, BOOL *_Nonnull stop) {
         if ([view isKindOfClass:[UIView class]]) {
             [self lua_addSubview:view];
