3ca1960f92826cb7042cad3ffba8cea46aeac062
dai.dongpeng <swiftwithlove@163.com>
2020-04-28 15:37:32 +0800

    [MLN] fix 预加载兼容

--- MLN-iOS/MLN/Classes/Kit/MLNKitInstance.m
+++ MLN-iOS/MLN/Classes/Kit/MLNKitInstance.m
@@ -406,8 +406,9 @@
 
 - (void)createLuaCore
 {
-    _luaCore = [self.luaCoreBuilder getLuaCore];
-    if (!self.luaCore) {
+    if (self.luaCoreBuilder) {
+        _luaCore = [self.luaCoreBuilder getLuaCore];
+    } else {
         _luaCore = [[MLNLuaCore alloc] initWithLuaBundle:_currentBundle convertor:_convertorClass exporter:_exporterClass];
     }
     [_luaCore changeLuaBundle:self.currentBundle];
