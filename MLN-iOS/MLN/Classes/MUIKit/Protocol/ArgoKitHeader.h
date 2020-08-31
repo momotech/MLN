711f31b659b9883dcb0fc422348891863d4e9cd3
dai.dongpeng <swiftwithlove@163.com>
2020-08-31 14:21:58 +0800

    [ArgoBinding] 添加controller

---
+++ MLN-iOS/MLN/Classes/MUIKit/Protocol/ArgoKitHeader.h
@@ -0,0 +1,15 @@
+//
+//  ArgoKitHeader.h
+//  Pods
+//
+//  Created by Dongpeng Dai on 2020/8/28.
+//
+
+#ifndef ArgoKitHeader_h
+#define ArgoKitHeader_h
+
+#define ArgoKitInstance MLNUIKitInstance
+#define ArgoKitInstanceHandlersManager MLNUIKitInstanceHandlersManager
+#define ArgoExportProtocol MLNUIExportProtocol
+
+#endif /* ArgoKitHeader_h */
