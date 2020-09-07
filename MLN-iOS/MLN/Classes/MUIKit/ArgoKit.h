40570810e8fd26b55040bade0c77cdeec735798d
dai.dongpeng <swiftwithlove@163.com>
2020-09-07 10:17:53 +0800

    [ArgoBinding] 适配生成model

--- MLN-iOS/MLN/Classes/MUIKit/ArgoKit.h
+++ MLN-iOS/MLN/Classes/MUIKit/ArgoKit.h
@@ -9,7 +9,15 @@
 #define ArgoKit_h
 
 #import "MLNUIKit.h"
+#import "MLNUIHeader.h"
+
 #import "ArgoObservableMap.h"
 #import "ArgoObservableArray.h"
+
+#if OCPERF_USE_NEW_DB
+#else
+#define ArgoObservableMap NSObject
+#define ArgoObservableArray NSMutableArray
+#endif
 
 #endif /* ArgoKit_h */
